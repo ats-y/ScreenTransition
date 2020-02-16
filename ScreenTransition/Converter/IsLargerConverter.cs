22549758909c48d889defa75d9da421023e4cc39
ats-y <london.rightside@gmail.com>
2020-02-16 16:53:32 +0900

    ラベル前後の省略記号をスクロール位置によって表示制御。

---
+++ ScreenTransition/Converter/IsLargerConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ScreenTransition.Converter
+{
+    public class IsLargerConverter : IValueConverter
+    {
+        public IsLargerConverter()
+        {
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ScrollView target = parameter as ScrollView;
+            if (target == null) return false;
+
+            return ((double)value < (target.ContentSize.Width - target.Width));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ScreenTransition/Converter/IsSmallConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ScreenTransition.Converter
+{
+    public class IsSmallConverter : IValueConverter
+    {
+        public IsSmallConverter()
+        {
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // ScrollView
+            VisualElement ve = parameter as VisualElement;
+            if (ve == null) return false;
+
+            return ((double)value > ve.Width);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
