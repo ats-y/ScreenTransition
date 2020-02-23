b0badcc35681498a408b8649051997aa2f3d9f1d
ats-y <london.rightside@gmail.com>
2020-02-23 13:27:52 +0900

    省略記号の表示制御をトリガーからScrolledイベントに変更。

--- ScreenTransition/Controls/ScrollableLabel.xaml.cs
+++ ScreenTransition/Controls/ScrollableLabel.xaml.cs
@@ -13,16 +13,35 @@ namespace ScreenTransition.Controls
             InitializeComponent();
         }
 
+        #region FontSize BindableProperty
+        public static readonly BindableProperty FontSizeProperty =
+            BindableProperty.Create("FontSize", typeof(double), typeof(ScrollableLabel), null, BindingMode.TwoWay, propertyChanged: OnFontSizePropertyChanged);
+
+        private static void OnFontSizePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (!(bindable is ScrollableLabel scrollableLabel)) return;
+            scrollableLabel.TextDispLabel.FontSize = (double)newValue;
+            scrollableLabel.HeadTruncation.FontSize = (double)newValue;
+            scrollableLabel.TailTruncation.FontSize = (double)newValue;
+        }
+
+        [System.ComponentModel.TypeConverter(typeof(FontSizeConverter))]
+        public double FontSize
+        {
+            get { return (double)GetValue(FontSizeProperty); }
+            set { SetValue(FontSizeProperty, value); }
+        }
+        #endregion
+
         #region Text BindableProperty
+
         public static readonly BindableProperty TextProperty =
             BindableProperty.Create("Text", typeof(string), typeof(ScrollableLabel), null, BindingMode.TwoWay, propertyChanged: OnTextPropertyChanged);
 
         private static void OnTextPropertyChanged(BindableObject bindable, object oldValue, object newValue)
         {
-            ScrollableLabel scrollableLabel = bindable as ScrollableLabel;
-            if (scrollableLabel == null) return;
+            if (!(bindable is ScrollableLabel scrollableLabel)) return;
             scrollableLabel.TextDispLabel.Text = newValue as string;
-            //throw new NotImplementedException();
         }
 
         public string Text
@@ -31,43 +50,67 @@ namespace ScreenTransition.Controls
             set { SetValue(TextProperty, value); }
         }
 
-        void HeadTruncation_PropertyChanged(System.Object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-        }
         #endregion
 
+        /// <summary>
+        /// 省略記号プロパティ変化イベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         private void OnTruncationPropertyChanged(object sender, PropertyChangedEventArgs args)
         {
             if (args.PropertyName != "IsVisible") return;
             if (!(sender is VisualElement element)) return;
             if (element.IsVisible == false) return;
 
+            // 省略記号表示エリアの背景色を決める。
+            // ラベル文字列の上に省略記号をかぶせるので、透明でない背景色の親View探す。
             Color background = Color.Transparent;
             VisualElement search = element;
             while (search != null)
             {
-                if (search is NavigationPage)
-                {
-                    background = ((NavigationPage)search).BarBackgroundColor;
-                }
-                else
-                {
-                    background = search.BackgroundColor;
-                }
-
-                if (background.A > 0)
-                {
-                    break;
-                }
+                background = search.BackgroundColor;
+                if (background.A > 0) break;
                 search = search.Parent as VisualElement;
             }
             element.BackgroundColor = background;
         }
 
-        private void OnSizeChanged(object sender, EventArgs args)
+        /// <summary>
+        /// ラベルサイズ変更イベントハンドラ
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void OnLabelSizeChanged(object sender, EventArgs args)
         {
-            //Debug.WriteLine($"OnSizeChanged {TextDispLabel.Width}/{scrV.Width} ");
-            //TailTruncation.IsVisible = (TextDispLabel.Width - scrV.Width > 0);
+            // ラベルの初回表示で末端省略記号を表示するかどうか決める。
+            Debug.WriteLine($"OnLabelSizeChanged {TextDispLabel.Width}/{scrV.Width} ");
+            TailTruncation.IsVisible = (TextDispLabel.Width - scrV.Width > 0);
+        }
+
+        /// <summary>
+        /// スクロールイベントハンドラ
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void OnScrolled(object sender, ScrolledEventArgs args)
+        {
+            // パラメータチェック。
+            if (!(sender is ScrollView sv)) return;
+
+            // 先頭省略記号表示制御。
+            // スクロール位置が左端より右にあれば表示する。
+            HeadTruncation.IsVisible = (0 < args.ScrollX);
+
+            // 末端省略記号表示制御。
+            // スクロール位置が右端よりも左にあれば表示する。
+            bool isVisible = false;
+            double scrollSize = sv.ContentSize.Width - sv.Width;
+            if (0 < scrollSize && args.ScrollX < scrollSize)
+            {
+                isVisible = true;
+            }
+            TailTruncation.IsVisible = isVisible;
         }
     }
 }
